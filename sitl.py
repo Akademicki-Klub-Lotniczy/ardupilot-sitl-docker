5911a60753219decddee61e4f6643fa86e9f8b70
wint3rmute <mateusz.baczek1998@gmail.com>
2020-01-22 13:59:23 +0100

    rename

--- sitl.py
+++ sitl.py
@@ -3,26 +3,21 @@ import os
 
 parser = argparse.ArgumentParser(description='SITL Runner')
 
-parser.add_argument('--map', help='Show the map window', action='store_true')
-parser.add_argument('--console', help='Show the console window', action='store_true')
-parser.add_argument('-v', help='Choose vehicle' )
+parser.add_argument('-m', '--map', help='Show the map window', action='store_true')
+parser.add_argument('-c', '--console', help='Show the console window', action='store_true')
+parser.add_argument('-v', '--vehicle', help='Choose vehicle (ArduPlane or ArduCopter)', required=True )
 
 cli_args = parser.parse_args()
 
-# `os.execvp` ignores the first arg, so there's a dummy
 docker_args = ['docker',
         'run',
         '-it',
         '--net=host',
         '--env=DISPLAY',
         '--volume',
-        '/home/wint3rmute/.Xauthority:/home/akl/.Xauthority:rw',
+        f'{os.environ["HOME"]}/.Xauthority:/home/akl/.Xauthority:rw',
         'wnt3rmute/ardupilot-sitl',
         './sim_vehicle.py',
-        #'--console',
-        #'--map',
-        #'-v',
-        #'ArduCopter',
         '-N',
 ]
 
@@ -32,11 +27,8 @@ if cli_args.map:
 if cli_args.console:
     docker_args.append('--console')
 
-if not cli_args.v:
-    print('no vehicle set!')
-    exit()
-else:
-    vehicle = cli_args.v.strip()
+if cli_args.vehicle:
+    vehicle = cli_args.vehicle.strip()
     print(vehicle)
     docker_args.append('-v')
     docker_args.append(f'{vehicle}')
